e68f6fe47b7c706094b1c1589c05c70516778eeb
Artem <ar.doroxoff@gmail.com>
2024-09-01 11:50:28 +0300

    start

---
+++ Autorization.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hw_registr;
+
+namespace Hw_registr
+{
+    public static class AuthorizationService
+    {
+        private static void ShowMainMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Главное меню:");
+                Console.WriteLine("1. Опция 1");
+                Console.WriteLine("2. Опция 2");
+                Console.WriteLine("3. Выход");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        // Логика для Опции 1
+                        break;
+                    case "2":
+                        // Логика для Опции 2
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.WriteLine("Некорректный выбор, попробуйте снова.");
+                        break;
+                }
+            }
+        }
+        public static void AuthorizeUser()
+        {
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+
+            Console.Write("Введите пароль: ");
+            string password = Console.ReadLine();
+
+            using (var db = new ApplicationContext())
+            {
+                var user = db.Registrations.SingleOrDefault(u => u.Login == login);
+                if (user != null && RegistrService.VerifyHashedPassword(user.Password, password))
+                {
+                    Console.WriteLine("Авторизация успешна!");
+                    ShowMainMenu();
+                }
+                else
+                {
+                    Console.WriteLine("Неверный логин или пароль.");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hw_registr
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBaseService dbService = new DataBaseService();
+            dbService.EnsurePopulate();
+
+            Console.WriteLine("Выберите действие:");
+            Console.WriteLine("1. Регистрация");
+            Console.WriteLine("2. Авторизация");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    RegistrService.RegisterUser();
+                    break;
+                case "2":
+                    AuthorizationService.AuthorizeUser();
+                    break;
+                default:
+                    Console.WriteLine("Некорректный выбор.");
+                    break;
+            }
+        }
+    }
+
+    public class Registration
+    { 
+        public int Id { get; set; }
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    //public class Authorization
+    //{
+        
+    //}
+
+
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Registration> Registrations { get; set; } = null;
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-Q2JP8KP;Database=Motel;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+    public class DataBaseService
+    { 
+        public void EnsurePopulate()
+        {
+            using (var db = new ApplicationContext()) 
+            { 
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+        }
+    }
+
+
+}
